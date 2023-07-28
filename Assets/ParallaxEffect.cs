67dbb717a4f2b781099c2bdf03f8965b9a922f1a
AnNgo <ngohoangan312001@gmail.com>
2023-07-29 00:35:51 +0700

    add paralax background, walk, run, ilde animation and flip direction

--- Assets/ParallaxEffect.cs
+++ Assets/ParallaxEffect.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
 {
+    public Camera camera;
+    public Transform followTarget;
+
+    //start position of the paralax game object
+    Vector2 startingPosition;
+
+    //start Z value of the paralax game object
+    float startingZ;
+
+    // "=>" syntax mean update itseft every frame so it no need to be in update() method
+    Vector2 camMoveSinceStart => (Vector2)camera.transform.position - startingPosition ;
+
+    float zDistanceFromTarget => transform.position.z - followTarget.transform.position.z;
+
+    float clippingPlane => (camera.transform.position.z + (zDistanceFromTarget > 0 ? camera.farClipPlane : camera.nearClipPlane));
+
+    // the further the object from the player, the slower ParalaxEffect object will move
+    float paralaxFactor => Mathf.Abs(zDistanceFromTarget) / clippingPlane;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
+        startingZ = transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //when the target move, move the parallax object the same distance times a multiplier
+        Vector2 newPosition = startingPosition + camMoveSinceStart * paralaxFactor;
+
+        //the X/Y position changes based on target travel speed times the parallax factor, z stay consistent
+        transform.position = new Vector3(newPosition.x, newPosition.y, startingZ);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,14 +8,83 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+
+    public float currentMoveSpeed
+    {
+        get
+        { 
+            if(IsMoving)
+            {
+                if(IsRunning)
+                {
+                    return runSpeed;
+                }
+                else
+                {
+                    return walkSpeed;
+                }
+            }
+            else
+            {
+                // 0 = not moving
+                return 0;
+            }
+        }
+    }
+
+
     Vector2 moveInput;
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool IsMoving { get 
+        { 
+            return _isMoving; 
+        } 
+        private set 
+        { 
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        } 
+    }
+
+    [SerializeField]
+    private bool _isRunning = false;
+    public bool IsRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
+
+    public bool _isFacingRight = true;
+    public bool IsFacingRight { get
+        {
+            return _isFacingRight;
+        } private set { 
+            if(_isFacingRight != value)
+            {
+                //Flip the local scal to make player face oposite
+                transform.localScale *= new Vector2(-1, 1); 
+            }
+
+            _isFacingRight = value;
+        } 
+    }
+
     Rigidbody2D rb;
-    public bool isMoving;
-
-
+    Animator animator;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -30,12 +100,40 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(moveInput.x * currentMoveSpeed, rb.velocity.y);
     }
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
 
-        isMoving = moveInput != Vector2.zero;
+        IsMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput);
+    }
+
+    private void SetFacingDirection(Vector2 moveInput)
+    {
+        if (moveInput.x > 0 && !IsFacingRight) 
+        {
+            // Face the right
+            IsFacingRight = true;
+        }
+        else if (moveInput.x < 0 && IsFacingRight) 
+        {
+            // Face the left
+            IsFacingRight = false;
+        }
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if(context.started) 
+        {
+            IsRunning = true;
+        }
+        else if (context.canceled)
+        {
+            IsRunning = false;
+        }
     }
 }
